e187bd826fc7299446005fd9a409806c17497038
Matt Coy <fluffytoycoy@gmail.com>
2019-05-06 10:43:11 -0500

    minor JS changes to navbar functions and text color change

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,24 @@
 
 import { getWindowWidth } from './util.js';
+
 window.onload = function (){
-const navBarToggle = document.getElementById('navbar-toggle');
-const navBar = document.getElementById('navbar');
-const moblieBreakPoint = 655;
 
-navBarToggle.addEventListener('click', () =>{
-  navBar.classList.toggle('active');
-});
+  const navBarToggle = document.getElementById('navbar-toggle');
+  const navBar = document.getElementById('navbar');
+  const dropdown = document.getElementById('dropdown');
 
-window.addEventListener('resize',()=>{
-  if(getWindowWidth() > moblieBreakPoint && navBar.classList.contains('active')){
-    navBar.classList.remove('active');
-  }
-});
+  const moblieBreakPoint = 655;
+
+  navBarToggle.addEventListener('click', () =>{
+    dropdown.classList.toggle('active');
+    navBar.classList.toggle('expand');
+  });
+
+  window.addEventListener('resize', ()=>{
+    if(getWindowWidth() > moblieBreakPoint && dropdown.classList.contains('active')){
+      navBar.classList.remove('expand');
+      dropdown.classList.remove('active');
+
+    }
+  });
 };
